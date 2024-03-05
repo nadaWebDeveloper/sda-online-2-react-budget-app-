f373b5320bbef8f3142851bfebe80eb7c49a5128
nada <nadayahya2020@hotmail.com>
2024-03-05 08:54:32 -0800

    Done income sources, value and date

--- src/components/Income.tsx
+++ src/components/Income.tsx
@@ -1,30 +1,126 @@
-import React from "react";
+import { ChangeEvent, FormEvent, useMemo, useState } from "react";
 
+import { v4 as uuidv4 } from "uuid";
+import { format } from "date-fns";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrash, faEdit } from "@fortawesome/free-solid-svg-icons";
+
+import { typeIncome } from "./Types";
+
+const income: typeIncome = {
+  id: "",
+  incomeSource: "",
+  incomeAmount: 0,
+  incomeDate: "",
+};
 function Income() {
+  const [incomeArray, setIncomeArray] = useState<typeIncome[]>([]);
+  const [incomeItem, setIncomeItem] = useState(income);
+  const totalAmount = useMemo(() => {
+    return incomeArray.reduce((total, current) => {
+      return total + parseInt(current.incomeAmount.toString())
+    }, 0);
+  }, [incomeArray]);
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const { value, name } = event.target;
+    setIncomeItem((prevIncome) => ({
+      ...prevIncome,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    if (
+      incomeItem.incomeAmount &&
+      incomeItem.incomeSource &&
+      incomeItem.incomeDate
+    ) {
+      if (incomeItem.incomeAmount > 0) {
+        const neeIncome = {
+          id: uuidv4(),
+          incomeSource: incomeItem.incomeSource,
+          incomeAmount: incomeItem.incomeAmount,
+          incomeDate: incomeItem.incomeDate,
+        };
+        setIncomeArray((prevItem) => [...prevItem, neeIncome]);
+        setIncomeItem(income);
+        alert("Added New Income");
+      } else {
+        alert("Enter Positave Number");
+      }
+    }
+  };
+
+  const handleDelete = (incomeId: string | undefined) => {};
+
   return (
     <>
       <div>
-        <form action="">
+        <form onSubmit={handleSubmit}>
           <div>
-            <label htmlFor="income">Income Source:</label>
+            <label htmlFor="incomeSource">Income Source:</label>
             <input
               type="text"
-              value="0"
-              name="income"
-              id="income"
+              value={incomeItem.incomeSource}
+              name="incomeSource"
+              id="incomeSource"
               placeholder="Salary"
+              onChange={handleChange}
+              required
             />
           </div>
           <div>
-            <label htmlFor="amount">Amount Income</label>
-            <input type="number" name="amount" id="amount" />
+            <label htmlFor="incomeAmount">Amount Income</label>
+            <input
+              type="number"
+              name="incomeAmount"
+              id="incomeAmount"
+              value={incomeItem.incomeAmount}
+              onChange={handleChange}
+              required
+            />
           </div>
           <div>
-            <label htmlFor="date">Date Income:</label>
-            <input type="date" name="date" id="date" />
+            <label htmlFor="incomeDate">Date Income:</label>
+            <input
+              type="date"
+              name="incomeDate"
+              id="incomeDate"
+              value={incomeItem.incomeDate}
+              onChange={handleChange}
+              required
+            />
           </div>
           <button type="submit">Add Income</button>
         </form>
+      </div>
+      <div>
+        {incomeArray.length > 0 ? <p>Total Of Amount: {totalAmount} RS</p> : ""}
+      </div>
+      <div>
+        {incomeArray.length > 0 ? (
+          incomeArray.map((income) => {
+            return (
+              <ul key={income.id}>
+                <li key={income.id}>
+                  {income.incomeSource}: {income.incomeAmount} RS On
+                  {format(new Date(income.incomeDate), "eee - d MMM yyyy")}
+                  <span onClick={() => handleDelete(income.id)}>
+                    <FontAwesomeIcon icon={faTrash} />
+                  </span>
+                  <span>
+                    <FontAwesomeIcon icon={faEdit} />
+                  </span>
+                </li>
+              </ul>
+            );
+          })
+        ) : (
+          <h3>----------------</h3>
+        )}
       </div>
     </>
   );
---
+++ src/components/Types.tsx
@@ -0,0 +1,13 @@
+export type typeIncome = {
+    id?:string;
+    incomeSource: string;
+    incomeAmount:number;
+    incomeDate: string;
+}
+
+export type typeExpense ={
+    d?:string;
+    expenseSource: string;
+    expenseAmount: number;
+    expenseDate: string;
+}
