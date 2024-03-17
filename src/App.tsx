56e0b026667b4ad0a3fd246aeaab46566cbdf778
nada <nadayahya2020@hotmail.com>
2024-03-16 19:54:36 -0700

    Done navbar section

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import Budget from "./components/Budget";
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <>
       <BrowserRouter>
+      <Navbar/>
         <Routes>
-          <Route path="/" element={<Budget />} />
+        <Route path="/" element={<Home />} />
+          <Route path="/budget-app" element={<Budget />} />
         </Routes>
       </BrowserRouter>
     </>
